30179aa16de84d2218fb092a3d131f9bd772e24f
Joana Duarte <joanarpcribeiro@yahoo.com>
2019-05-14 12:19:46 +0100

    Task 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      celebrities: []
+    }
+  }
+  componentDidMount() {
+    axios.get('https://api.themoviedb.org/3/person/popular?page=2&api_key=641fef92d73fd584f3b23412e2c4fada')
+      .then(response => {
+        const celebrities = response.data.results /*response*/
+        console.log("FIRST SET OF CELEBRITIES", celebrities)
+        this.setState({
+          celebrities: celebrities
+        })
+        console.log("SECOND SET OF CELEBRITIES", celebrities)
+      })
+      .catch(err => console.log(err))
+  }
+  render(){
+    console.log("THIRD SET OF CELEBRITIES", this.state.celebrities)
+    return (
+      <div className="App">
+        <ul>
+        {this.state.celebrities.map( celebrity =>
+          //console.log("DATA DETAILS",celebrity.name)
+           <li key={celebrity.name}><img className="celebImg" src={`https://image.tmdb.org/t/p/w185/${celebrity.profile_path}`}/>{celebrity.name}</li>
+           )}
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default App;
